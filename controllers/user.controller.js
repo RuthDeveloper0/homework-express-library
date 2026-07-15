d47e30a8125fc2a09f4a6b566b97365356b0a3f8
RuthDeveloper0 <269273890+RuthDeveloper0@users.noreply.github.com>
2026-07-15 19:22:42 +0300

    Refactor to MVC: move business logic from routes to book and user controllers

---
+++ controllers/user.controller.js
@@ -0,0 +1,45 @@
+const { users } = require('../db.js');
+
+
+const getAllUsers = (req, res) => {
+    res.json(users);
+};
+
+const signUpUser = (req, res) => {
+    const { username, email, password } = req.body;
+
+    const userExists = users.find(u => u.email === email);
+    if (userExists) {
+        return res.status(400).send('משתמש עם אימייל זה כבר קיים במערכת');
+    }
+
+    const newUser = {
+        id: users.length + 101,
+        username,
+        email,
+        password,
+        borrowedBooks: [] 
+    };
+
+    users.push(newUser);
+    res.status(201).json({ message: "המשתמש נרשם בהצלחה", user: newUser });
+};
+
+
+const signInUser = (req, res) => {
+    const { email, password } = req.body;
+    
+    const user = users.find(u => u.email === email && u.password === password);
+    
+    if (!user) {
+        return res.status(401).send('אימייל או סיסמה שגויים');
+    }
+
+    res.json({ message: "התחברות הצליחה", username: user.username });
+};
+
+module.exports = {
+    getAllUsers,
+    signUpUser,
+    signInUser
+};
