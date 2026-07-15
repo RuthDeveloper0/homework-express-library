020a547af014a74903ff2d1b7049b7a1f11f2bc0
RuthDeveloper0 <269273890+RuthDeveloper0@users.noreply.github.com>
2026-07-15 18:27:21 +0300

    Complete CRUD and borrow/return operations for books in step 3

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const { books } = require('./db.js');
 
 
 const app = express();
+app.use(express.json());
 const P = 5000;
 
 app.get('/', (req, res) => {
@@ -13,6 +14,93 @@ app.get('/', (req, res) => {
 app.get('/books', (req, res) => {
     res.json(books);
 });
+
+app.get('/books/:id', (req, res) => {
+
+    const book = books.find(b => b.id == req.params.id);
+
+    if (book) {
+        res.json(book); 
+    } else {
+        res.status(404).send('not found the book');
+    }
+});
+
+
+app.post('/books', (req, res) => {
+    const newBook = req.body; 
+    books.push(newBook);    
+    res.status(201).json(newBook); 
+});
+
+
+app.post('/books/:id', (req, res) => {
+    const bookId = parseInt(req.params.id); 
+    const book = books.find(b => b.id === bookId); 
+
+
+    if (!book) {
+        return res.status(404).send('הספר לא נמצא');
+    }
+
+    book.title = req.body.title || book.title;
+    book.category = req.body.category || book.category;
+    book.price = req.body.price || book.price;
+
+    res.json({ message: "הספר עודכן בהצלחה", book });
+});
+
+
+app.post('/books/borrow/:id', (req, res) => {
+    const bookId = parseInt(req.params.id);
+    const { customerId } = req.body; 
+
+
+    const book = books.find(b => b.id === bookId);
+
+    if (!book) {
+        return res.status(404).send('הספר לא נמצא');
+    }
+
+
+    if (book.isBorrowed) {
+        return res.status(400).send('הספר כבר מושאל ולא ניתן להשאיל אותו שוב');
+    }
+
+
+    book.isBorrowed = true;
+
+
+    const newBorrow = {
+        borrowDate: new Date().toISOString().split('T')[0], 
+        customerId: parseInt(customerId)
+    };
+
+    book.borrowHistory.push(newBorrow);
+
+    res.json({ message: "הספר הושאל בהצלחה", book });
+});
+
+
+app.post('/books/return/:id', (req, res) => {
+    const bookId = parseInt(req.params.id);
+
+    const book = books.find(b => b.id === bookId);
+
+    if (!book) {
+        return res.status(404).send('הספר לא נמצא');
+    }
+
+    if (!book.isBorrowed) {
+        return res.status(400).send('הספר אינו נמצא במצב מושאל, לכן לא ניתן להחזיר אותו');
+    }
+
+
+    book.isBorrowed = false;
+
+    res.json({ message: "הספר הוחזר בהצלחה ", book });
+});
+
 
 app.listen(P, () => {
     console.log(`Server running on port ${P}`);
