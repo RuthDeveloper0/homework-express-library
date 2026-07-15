9925fffcdfaa622db0587777df58f5963ef87e22
RuthDeveloper0 <269273890+RuthDeveloper0@users.noreply.github.com>
2026-07-15 19:26:38 +0300

    change from post to put

--- routes/book.route.js
+++ routes/book.route.js
@@ -8,7 +8,7 @@ const bookController = require('../controllers/book.controller.js');
 router.get('/', bookController.getAllBooks);
 router.get('/:id', bookController.getBookById);
 router.post('/', bookController.createBook);
-router.post('/:id', bookController.updateBook);
+router.put('/:id', bookController.updateBook);
 router.post('/borrow/:id', bookController.borrowBook);
 router.post('/return/:id', bookController.returnBook);
 
